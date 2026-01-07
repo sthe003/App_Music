b31390f9c4af7fd0d420173aa6a79c882f6a7d86
Sthefanie <sthefaniefernanda19@gmail.com>
2026-01-07 19:59:45 -0300

    first commit

---
+++ src/br/com/minhasmusicas/principal/Principal.java
@@ -0,0 +1,30 @@
+package br.com.minhasmusicas.principal;
+
+import br.com.minhasmusicas.modelos.Musica;
+import br.com.minhasmusicas.modelos.Podcast;
+
+public class Principal {
+    public static void main(String[] args) {
+        Musica minhaMusica = new Musica();
+        minhaMusica.setTitulo("Exit Music");
+        minhaMusica.setArtista("Radiohead");
+
+        for (int i = 0; i < 1000; i++){
+            minhaMusica.reproduz();
+        }
+        for (int i = 0; i < 50; i++){
+            minhaMusica.curte();
+        }
+
+        Podcast meuPodcast = new Podcast();
+        meuPodcast.setTitulo("BolhaDev");
+        meuPodcast.setHost("Marcus Mendes");
+
+        for (int i = 0; i < 500; i++) {
+            meuPodcast.reproduz();
+        }
+        for (int i = 0; i < 10; i++) {
+            meuPodcast.curte();
+        }
+    }
+}
